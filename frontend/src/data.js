dd1248e9f4e7f50a8f2bf0cea7d0ec3af809be6f
Clife-Developer <clife.united@gmail.com>
2021-03-11 09:30:28 +0200

    created and modified

---
+++ frontend/src/data.js
@@ -0,0 +1,80 @@
+const data={
+    products:[
+        {
+        _id:1,
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:"./images/p1.jpg",
+        price:120,
+        brand:"Nike",
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+
+    },
+     {
+        _id:2,
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:"./images/p2.jpg",
+        price:120,
+        brand:"Truwoths",
+        rating:4.5,
+        numReviews:15,
+        description:"high quality product"
+
+    },
+     {
+        _id:3,
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:"./images/p3.jpg",
+        price:140,
+        brand:"Adidas",
+        rating:4.5,
+        numReviews:12,
+        description:"high quality product"
+
+     },
+      {
+        _id:4,
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:"./images/p4.jpg",
+        price:110,
+        brand:"Puma",
+        rating:4.5,
+        numReviews:8,
+        description:"high quality product"
+
+    },
+     {
+        _id:5,
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:"./images/p5.jpg",
+        price:130,
+        brand:"Lacoste",
+        rating:4.5,
+        numReviews:30,
+        description:"high quality product"
+
+    },
+     {
+        _id:6,
+        name:"Nike Slim Shirt",
+        category:"Shirts",
+        image:"./images/p6.jpg",
+        price:140,
+        brand:"Nike",
+        rating:4.5,
+        numReviews:11,
+        description:"high quality product"
+
+    }
+    ]
+}
+    
+
+
+export default data;
