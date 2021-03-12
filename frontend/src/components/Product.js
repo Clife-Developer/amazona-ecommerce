02a54dcc7440190e7ca3f585d2ad64e6594c0b6b
Clife-Developer <clife.united@gmail.com>
2021-03-12 07:45:11 +0200

    modified

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Rating from './Rating'
 function Product({product}) {
     return (
+        // Product Component for displaying Products
               <div className="card" key={product._id}>
                     <a href={`/product/${product._id}`}>
                         <img className="medium" src={product.image} alt={product.name}/>
