152c4a88c70e5e584b4874c427748e17e9ed73af
Clife-Developer <clife.united@gmail.com>
2021-03-19 08:22:21 +0200

    creating register endpoint

--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -25,21 +25,26 @@ router.post('/signin',async(req,res)=>{
 })
 
 
-router.post('/singup',async(req,res)=>{
+router.post('/register',async(req,res)=>{
     try {
-        const user=new User(req.body)
+        const user=new User({
+            name:req.body.name,
+            email:req.body.email,
+            password:await bcrypt.hash(req.body.password,8)
+        })
+         await user.save();
+         res.status(200).send(
+            {
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            token:generateToken(user)
+        })
     } catch (error) {
         res.status(404).send(error.message? {error:error.message}:error)
     }
+
 })
 
-router.post('/seed',async(req,res)=>{
-    try {
-        const user=new User(req.body)
-        await user.save()
-        res.status(200).send(user)
-    } catch (error) {
-        res.status(404).send(error.message? error.message:error)
-    }
-})
 module.exports=router;
